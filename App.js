cffa31ad3d12d311d7df84fc4f7c42e9bc0b0616
ajaphid <aamonett@andrew.cmu.edu>
2024-06-30 20:38:31 -0400

    basic starter template

--- App.js
+++ App.js
@@ -3,4 +3,21 @@ import HomeScreen from './src/pages/HomeScreen';
 import BreatheScreen from './src/pages/BreatheScreen';
 import TutorialScreen from './src/pages/TutorialScreen';
 import AboutDrRowScreen from './src/pages/AboutDrRowScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
+        <Stack.Screen name="Breathe" component={BreatheScreen} options={{ title: 'Breathe' }} />
+        <Stack.Screen name="Tutorial" component={TutorialScreen} options={{ title: 'Tutorial' }} />
+        <Stack.Screen name="About" component={AboutDrRowScreen} options={{ title: 'About Dr. Row' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
