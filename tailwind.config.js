052f5103f9bf75f08426c8d91d74b96f504804f8
Evelynn Chen <evelynnc@andrew.cmu.edu>
2025-03-06 23:48:18 +0100

    configured tailwind and moved breathe screen to homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
     content: [
       "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-      extend: {},
+      colors: {
+        ...colors,
+        'darkBrown': '#5C4036',
+        'darkRed': '#963817',
+      }
     },
     plugins: [],
   }
