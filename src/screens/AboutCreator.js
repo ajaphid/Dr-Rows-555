2548ad62e9885db19e5ce2da4403a2e26b396dd8
Evelynn Chen <evelynnc@andrew.cmu.edu>
2025-03-08 02:38:36 +0100

    refactored about component and added in testimonials placeholder

---
+++ src/screens/AboutCreator.js
@@ -0,0 +1,28 @@
+import DrRowHeadshot from '../assets/images/Dr. Rowena.jpeg';
+
+export default function AboutCreator() {
+
+  return (
+    <div className="mb-12">
+      <h1 className="mx-auto py-8">About the creator</h1>
+      <div className="space-y-12">
+        <img
+          src={DrRowHeadshot}
+          alt="Headshot"
+          className="mx-auto border-4 border-darkBrown w-2/3 lg:w-1/2 h-fit"
+        />
+        <div>
+          <div className="info-header">Who is Dr. Row?</div>
+          <p>
+            Rowena D. Pingul-Ravano, MD, FAAFP (Dr. Row) is board
+            certified by the  American Board of Family Medicine and
+            is a Fellow of the American  Academy of Family Practice. She
+            currently serves as the Chief of Family Medicine at UPMC Magee
+            Women’s Hospital and is an Assistant Professor at  the University
+            of Pittsburgh.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/screens/Testimonials.js
@@ -0,0 +1,8 @@
+export default function Testimonials() {
+    return (
+        <>
+            <h1 className="mx-auto py-8">What patients say</h1>
+            <p>Testimonials to be implemented</p>
+        </>
+    )
+}
