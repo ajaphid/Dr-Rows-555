3b01dee56097f0dbf899107d20072c74429a5565
Evelynn Chen <evelynnc@andrew.cmu.edu>
2025-03-23 16:38:43 -0400

    added link to download 555 prescription card

--- src/screens/InstructionsPopup.js
+++ src/screens/InstructionsPopup.js
@@ -9,13 +9,12 @@ const Popup = ({ isVisible, onClose }) => {
             onClick={onClose}
           >
             {/* X svg from heroicons.com */}
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="size-6">
-              <path stroke-linecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
-  
           </button>
           <h2 className="text-3xl font-bold mb-4 font-['Maragsa_Display']">Instructions</h2>
-          <div>
+          <div className="mb-4">
             <ol className="list-decimal pl-4 mb-4">
               <li>Inhale for 5 seconds</li>
               <li>Hold your breath for 5 seconds</li>
@@ -23,9 +22,15 @@ const Popup = ({ isVisible, onClose }) => {
               <li>Hold your breath for 5 seconds</li>
               <li>Relax your breath; breathe normally for 5 seconds</li>
             </ol>
-  
             Repeat 5x, or until calm.
           </div>
+          <a
+            href="/prescription-card.png"
+            download="prescription-card.png"
+            className="absolute bottom-4 left-4 text-darkRed underline text-sm hover:text-darkBrown"
+          >
+            Click here to download a 555 prescription card
+          </a>
         </div>
       </div>
     );
